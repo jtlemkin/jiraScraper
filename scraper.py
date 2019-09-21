3b6ea3756c0912a1c2236f32f40c8fd7f8966b85
jtlemkin <jtlemkin@gmail.com>
2019-09-21 09:30:21 -0700

    parse severity of issue

--- scraper.py
+++ scraper.py
@@ -7,4 +7,5 @@ resp = requests.get(url)
 if resp.status_code != 200:
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 
-print(json.dumps(resp.json(), indent=4))
+#print(json.dumps(resp.json(), indent=4))
+print('{}'.format(resp.json()["fields"]["priority"]["name"]))
