f87c1baec3ff9b02460a096106e2d6cf73c7c8c2
jtlemkin <jtlemkin@gmail.com>
2019-09-21 11:13:17 -0700

    refactor code into methods

--- scraper.py
+++ scraper.py
@@ -3,21 +3,30 @@ import requests
 from dateutil.parser import parse
 from datetime import timedelta
 
+def parseIssuesToCSV(url):
+    resp = requests.get(url)
+
+    if resp.status_code != 200:
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+
+    #print(json.dumps(resp.json(), indent=4))
+    priority = getPriority(resp.json())
+    print(priority)
+
+    timeToFix = getTimeToFix(resp.json())
+    print(timeToFix)
+
+def getPriority(respJson):
+    return respJson["fields"]["priority"]["name"]
+
+def getTimeToFix(respJson):
+    resolutionDate = parse(respJson["fields"]["resolutiondate"])
+    creationDate = parse(respJson["fields"]["created"])
+
+    timeToFix = resolutionDate - creationDate
+    timeToFixInDays = timeToFix / timedelta(days=1)
+
+    return timeToFixInDays
+
 url = "https://issues.apache.org/jira/rest/api/latest/issue/ACCUMULO-1?"
-resp = requests.get(url)
-
-if resp.status_code != 200:
-    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-
-#print(json.dumps(resp.json(), indent=4))
-#print('{}'.format(resp.json()["fields"]["priority"]["name"]))
-
-resolutionDate = parse(resp.json()["fields"]["resolutiondate"])
-print('{}'.format(resolutionDate))
-
-creationDate = parse(resp.json()["fields"]["created"])
-print('{}'.format(creationDate))
-
-timeToFix = resolutionDate - creationDate
-timeToFixInDays = timeToFix / timedelta(days=1)
-print(timeToFixInDays)
+parseIssuesToCSV(url)
