ed74a8a8819a86a92e8e681d67fc57f1f2384a4d
jtlemkin <jtlemkin@gmail.com>
2019-09-21 11:07:11 -0700

    parse out time to fix bug

--- scraper.py
+++ scraper.py
@@ -1,11 +1,23 @@
 import json
 import requests
+from dateutil.parser import parse
+from datetime import timedelta
 
-url = "https://issues.apache.org/jira/rest/api/latest/issue/ACCUMULO-1"
+url = "https://issues.apache.org/jira/rest/api/latest/issue/ACCUMULO-1?"
 resp = requests.get(url)
 
 if resp.status_code != 200:
     raise ApiError('GET /tasks/ {}'.format(resp.status_code))
 
 #print(json.dumps(resp.json(), indent=4))
-print('{}'.format(resp.json()["fields"]["priority"]["name"]))
+#print('{}'.format(resp.json()["fields"]["priority"]["name"]))
+
+resolutionDate = parse(resp.json()["fields"]["resolutiondate"])
+print('{}'.format(resolutionDate))
+
+creationDate = parse(resp.json()["fields"]["created"])
+print('{}'.format(creationDate))
+
+timeToFix = resolutionDate - creationDate
+timeToFixInDays = timeToFix / timedelta(days=1)
+print(timeToFixInDays)
