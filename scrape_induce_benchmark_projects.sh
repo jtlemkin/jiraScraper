87821b3bf1651d1ec824fdc463b6e1c802f67987
jtlemkin <jtlemkin@gmail.com>
2019-09-26 13:21:33 -0700

    added shell script to process projects in parallel

--- scrape_induce_benchmark_projects.sh
+++ scrape_induce_benchmark_projects.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
 projects=("ACCUMULO" "AMBARI" "HADOOP" "JCR" "LUCENE" "OOZIE")
+
+for project in "${projects[@]}"
+do
+    echo "Processing ${project}"
+    nohup python3 scraper.py $project &
+done
